b6abfc13d7da4ebcc8338964c24bcdb6ddfe82f0
zykaaal <86550397+zykaaal@users.noreply.github.com>
2021-06-27 13:00:53 +0300

    Create ping.js

---
+++ komutlar/ping.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args, color, prefix) => {
+    const zykaaal = new Discord.MessageEmbed()
+    .setAuthor(message.author.username, message.author.avatarURL())
+    .setDescription(`**Pingim:** ${client.ws.ing}`)
+    .setFooter(`Developed by zykaaal.`)
+
+    message.channel.send(zykaaal)
+
+
+}
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['ping','ping'],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'ping'
+  };
